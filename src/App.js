bfb770cfaf55e6437eb0b6b02ae8d82e545054ee
AliElsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2023-03-04 13:40:35 +0200

    Use Lazy loading and Suspense to wait for data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useRef } from "react";
-import TodoApp from "./components/TodoApp/TodoApp";
+import React, { lazy, Suspense, useEffect, useRef } from "react";
+// import TodoApp from "./components/TodoApp/TodoApp";
 import "./App.css";
 
-function App() {
-  const inputRef = useRef(null);
+const TodoApp = lazy(() => import("./components/TodoApp/TodoApp"))
 
-  useEffect(() => {
-    inputRef.current.focus();
-  }, [])
+function App() {
+  // const inputRef = useRef(null);
+
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, [])
   
   return (
     <div className="App">
-      <TodoApp ref={inputRef} textVal="To do"/>
+      <Suspense fallback={<div>loading ...</div>}>
+        <TodoApp textVal="To do"/>
+      </Suspense>
     </div>
   );
 }
